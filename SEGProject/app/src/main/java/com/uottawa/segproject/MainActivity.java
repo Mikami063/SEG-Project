cca3203c27c89b73ee761b7b60db6ca3d0119bf8
Qingyun Yang <qyang063@uottawa.ca>
2022-10-16 14:34:04 -0400

    database demo for inserting is done

--- SEGProject/app/src/main/java/com/uottawa/segproject/MainActivity.java
+++ SEGProject/app/src/main/java/com/uottawa/segproject/MainActivity.java
@@ -12,26 +12,31 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseReference accountsDbRef;
+    EditText etName,etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    DatabaseReference accountsDbRef;
-    EditText etName=(EditText) findViewById(R.id.TextName);
-    EditText etPassword=(EditText) findViewById(R.id.TextPassword);
 
+    //summit the data to database
     public void btnSummitClick(View view){
-        accountsDbRef= FirebaseDatabase.getInstance().getReference().child("Accounts");
+        etName=(EditText) findViewById(R.id.TextName);
+        etPassword=(EditText) findViewById(R.id.TextPassword);
+        accountsDbRef= FirebaseDatabase.getInstance().getReference().child("Accounts");// get collection [Accounts] on database
         insertAccountData();
     }
+    //helper method for btnSummitClick
     private void insertAccountData(){
         String name=etName.getText().toString();
         String password=etPassword.getText().toString();
+        TempAccount TA=new TempAccount(name,password);//a dummy class for testing
 
-        accountsDbRef.push().setValue(name,password);
-        Toast.makeText(MainActivity.this,"data inserted",Toast.LENGTH_SHORT).show();
+        accountsDbRef.push().setValue(TA);// add this dummy class to the database
+        Toast.makeText(MainActivity.this,"data inserted",Toast.LENGTH_SHORT).show();//show a success message if success
     }
 
 }
---
+++ SEGProject/app/src/main/java/com/uottawa/segproject/TempAccount.java
@@ -0,0 +1,17 @@
+package com.uottawa.segproject;
+
+public class TempAccount {
+    String name,password;
+    public TempAccount(String name,String password){
+        this.name=name;
+        this.password=password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
